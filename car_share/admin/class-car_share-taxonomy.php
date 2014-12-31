6d76bf32bd34164f45c0c58c095398debe445c46
ales <ales@ales-PC>
2014-12-31 12:11:02 +0100

    taxonomies..

--- car_share/admin/class-car_share-taxonomy.php
+++ car_share/admin/class-car_share-taxonomy.php
@@ -28,22 +28,17 @@ class Car_share_Taxonomy {
      * @var      string    $version    The version of this plugin.
      */
     public function __construct($car_share, $version) {
-
         $this->car_share = $car_share;
         $this->version = $version;
-         
+                 
         add_action('admin_menu', array($this, 'add_plugin_admin_menu'));
  
         // Add an action link pointing to the options page.
         $plugin_basename = plugin_basename(plugin_dir_path(__DIR__) . $this->car_share . '.php');
-        add_filter('plugin_action_links_' . $plugin_basename, array($this, 'add_action_links'));
-       
+        add_filter('plugin_action_links_' . $plugin_basename, array($this, 'add_action_links'));       
     }      
     
-    
-    
-    
-    
+        
     
 }
 
