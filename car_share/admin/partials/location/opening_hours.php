6db1accd82059a9a142fbeb828109390aff16aa3
ales <ales@ales-PC>
2015-01-02 16:22:25 +0100

    some location hours

--- car_share/admin/partials/location/opening_hours.php
+++ car_share/admin/partials/location/opening_hours.php
@@ -1,8 +1,58 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$days = array(
+    'Monday' => 'Monday',
+    'Tuesday' => 'Tuesday',
+    'Wednesday' => 'Wednesday',
+    'Thursday' => 'Thursday',
+    'Friday' => 'Friday',
+    'Saturday' => 'Saturday',
+    'Sunday' => 'Sunday',
+);
 
+?>
+
+<table id="opening-hours" class="overvies-table">   
+    <thead>
+        <tr>
+            <td></td>
+            <td><?php _e('From', $this->car_share) ?></td>
+            <td><?php _e('To', $this->car_share) ?></td>
+            <td><?php _e('Open', $this->car_share) ?></td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($days as $key => $label): ?>
+        <tr>
+            <td><?php _e($label, $this->car_share) ?></td>
+            <td>
+                <select name="open[<?php echo $key ?>][from][hour]">
+                <?php for($i = 0; $i < 25; $i++): ?>
+                    <option value="<?php echo $i ?>"><?php echo sprintf('%02d', $i) ?></option>
+                <?php endfor; ?>
+                </select> 
+                :
+                <select name="open[<?php echo $key ?>][from][min]">
+                <?php for($i = 0; $i < 60; $i++): ?>
+                    <option value="<?php echo $i ?>"><?php echo sprintf('%02d', $i) ?></option>
+                <?php endfor; ?>
+                </select>
+            </td>
+            <td>
+                <select name="open[<?php echo $key ?>][to][hour]">
+                <?php for($i = 0; $i < 25; $i++): ?>
+                    <option value="<?php echo $i ?>"><?php echo sprintf('%02d', $i) ?></option>
+                <?php endfor; ?>
+                </select> 
+                :
+                <select name="open[<?php echo $key ?>][to][min]">
+                <?php for($i = 0; $i < 60; $i++): ?>
+                    <option value="<?php echo $i ?>"><?php echo sprintf('%02d', $i) ?></option>
+                <?php endfor; ?>
+                </select>            
+            </td>
+            <td><input type="checkbox" name="open[<?php echo $key ?>][open]" value="1"></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
