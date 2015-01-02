fe8b0438e60e2fcd822071ccb1b075e11f68169f
brunofritsch <brunofritsch@10.0.0.4>
2015-01-02 17:14:14 +0100

    range datepicker with bootstrap default style

--- car_share/public/js/car_share-public.js
+++ car_share/public/js/car_share-public.js
@@ -1,38 +1,52 @@
-(function( $ ) {
-	'use strict';
-
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note that this assume you're going to use jQuery, so it prepares
-	 * the $ function reference to be used within the scope of this
-	 * function.
-	 *
-	 * From here, you're able to define handlers for when the DOM is
-	 * ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * Or when the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and so on.
-	 *
-	 * Remember that ideally, we should not attach any more than a single DOM-ready or window-load handler
-	 * for any particular page. Though other scripts in WordPress core, other plugins, and other themes may
-	 * be doing this, we should try to minimize doing that in our own work.
-	 */
-          
-        $(function() { 
-            $('#date').datepicker({
-              dateFormat : 'dd-mm-yy'
-            }); 
+(function($) {
+    'use strict';
+    /**
+     * All of the code for your public-facing JavaScript source
+     * should reside in this file.
+     *
+     * Note that this assume you're going to use jQuery, so it prepares
+     * the $ function reference to be used within the scope of this
+     * function.
+     *
+     * From here, you're able to define handlers for when the DOM is
+     * ready:
+     *
+     * $(function() {
+     *
+     * });
+     *
+     * Or when the window is loaded:
+     *
+     * $( window ).load(function() {
+     *
+     * });
+     *
+     * ...and so on.
+     *
+     * Remember that ideally, we should not attach any more than a single DOM-ready or window-load handler
+     * for any particular page. Though other scripts in WordPress core, other plugins, and other themes may
+     * be doing this, we should try to minimize doing that in our own work.
+     */
+ 
+    $(function() {
+        $("#car_datefrom").datepicker({
+            minDate: 0,
+            changeMonth: true,
+            numberOfMonths: 2,
+            onClose: function(selectedDate) {
+                $("#car_dateto").datepicker("option", "minDate", selectedDate);
+            }
         });
-         
-})( jQuery );
+        $("#car_dateto").datepicker({
+            minDate: 0,
+            changeMonth: true,
+            numberOfMonths: 2,
+            onClose: function(selectedDate) {
+                
+                $("#car_datefrom").datepicker("option", "maxDate", selectedDate);
+            
+            }
+        });
+    });
+ 
+})(jQuery);
