e24ddc05b28d472fa89b7871717b6d2235d2b1bc
brunofritsch <brunofritsch@10.0.0.4>
2015-01-02 10:26:22 +0100

    add class Car_share_Setting

--- car_share/admin/class-car_share-setting.php
+++ car_share/admin/class-car_share-setting.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The dashboard-specific functionality of the plugin.
  *
@@ -51,26 +50,33 @@ class Car_share_Setting {
 
         $this->car_share = $car_share;
         $this->version = $version;
-         
+
         add_action('admin_menu', array($this, 'add_plugin_admin_menu'));
- 
+
+
+        add_action('admin_init', array($this, 'register_settings'));
+
+
         // Add an action link pointing to the options page.
         $plugin_basename = plugin_basename(plugin_dir_path(__DIR__) . $this->car_share . '.php');
         add_filter('plugin_action_links_' . $plugin_basename, array($this, 'add_action_links'));
-       
-    }  
+
+        // Setting
+    }
+
     /**
-     * Register the administration menu for this plugin into the WordPress Dashboard menu. 
+     * Register the administration menu for this plugin into the WordPress Dashboard menu.
      */
     public function add_plugin_admin_menu() {
         /*
          * Add a settings page for this plugin to the Settings menu.
-         * 
+         *
          */
         $this->plugin_screen_hook_suffix = add_menu_page(
                 __('Car plugin settings', $this->car_share), __('Car plugin setting', $this->car_share), 'manage_options', $this->car_share, array($this, 'display_plugin_admin_page')
         );
-    } 
+    }
+
     /**
      * Render the settings page for this plugin.
      *
@@ -80,6 +86,69 @@ class Car_share_Setting {
         include_once( 'partials/car_share-admin-display.php' );
     }
 
+    function register_settings() {
+
+// add_settings_section( $id, $title, $callback, $page )
+        add_settings_section(
+                'main-settings-section', 'Main Settings', array($this, 'print_main_settings_section_info'), 'test-plugin-main-settings-section'
+        );
+
+// add_settings_field( $id, $title, $callback, $page, $section, $args )
+        add_settings_field(
+                'some-setting', 'Some Setting', array($this, 'create_input_some_setting'), 'test-plugin-main-settings-section', 'main-settings-section'
+        );
+
+// register_setting( $option_group, $option_name, $sanitize_callback )
+        register_setting('main-settings-group', 'test_plugin_main_settings_arraykey', array($this, 'plugin_main_settings_validate'));
+
+// add_settings_section( $id, $title, $callback, $page )
+        add_settings_section(
+                'additional-settings-section', 'Additional Settings', array($this, 'print_additional_settings_section_info'), 'test-plugin-additional-settings-section'
+        );
+
+// add_settings_field( $id, $title, $callback, $page, $section, $args )
+        add_settings_field(
+                'another-setting', 'Another Setting', array($this, 'create_input_another_setting'), 'test-plugin-additional-settings-section', 'additional-settings-section'
+        );
+
+// register_setting( $option_group, $option_name, $sanitize_callback )
+        register_setting('additional-settings-group', 'test_plugin_additonal_settings_arraykey', array($this, 'plugin_additional_settings_validate'));
+    }
+
+    function print_main_settings_section_info() {
+        echo '<p>Main Settings Description.</p>';
+    }
+
+    function create_input_some_setting() {
+        $options = get_option('test_plugin_main_settings_arraykey');
+        ?><input type="text" name="test_plugin_main_settings_arraykey[some-setting]" value="<?php echo $options['some-setting']; ?>" /><?php
+    }
+
+    function plugin_main_settings_validate($arr_input) {
+        $options = get_option('test_plugin_main_settings_arraykey');
+        $options['some-setting'] = trim($arr_input['some-setting']);
+        return $options;
+    }
+
+    function print_additional_settings_section_info() {
+        echo '<p>Additional Settings Description.</p>';
+    }
+
+    function create_input_another_setting() {
+        $options = get_option('test_plugin_additonal_settings_arraykey');
+        ?><input type="text" name="test_plugin_additonal_settings_arraykey[another-setting]" value="<?php echo $options['another-setting']; ?>" /><?php
+    }
+
+    function plugin_additional_settings_validate($arr_input) {
+        $options = get_option('test_plugin_additonal_settings_arraykey');
+        $options['another-setting'] = trim($arr_input['another-setting']);
+        return $options;
+    }
+
+    
+    
+    
+    
     /**
      * Add settings action link to the plugins page.
      *
--- car_share/admin/partials/car_share-admin-display.php
+++ car_share/admin/partials/car_share-admin-display.php
@@ -12,7 +12,45 @@
 */
 ?>
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+ 
+<div class="wrap">
+<?php screen_icon(); ?>
+    <h2>Settings</h2> 
+    <form method="post" action="options.php">
+    <?php
+// This prints out all hidden setting fields
+// settings_fields( $option_group )
+settings_fields( 'main-settings-group' );
+// do_settings_sections( $page )
+do_settings_sections( 'test-plugin-main-settings-section' );
+    ?>
+    <?php submit_button('Save Changes'); ?>
+    </form>
+ 
+    <form method="post" action="options.php">
+    <?php
+// This prints out all hidden setting fields
+// settings_fields( $option_group )
+settings_fields( 'additional-settings-group' );
+// do_settings_sections( $page )
+do_settings_sections( 'test-plugin-additional-settings-section' );
+    ?>
+    <?php submit_button('Save Changes'); ?>
+    </form>
+</div> 
 
+
+<?php /* zpracovani formulare pro vytvoreni stranek 
+ * 
+ * 
+ */
+
+
+
+
+
+ ?>
+  
 
 
 
@@ -21,20 +59,16 @@
 <div class="wrap"> 
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2> 
 	<!-- @TODO: Provide markup for your options page here. --> 
-	 <div class="section panel">  
-      
-      <form method="post" enctype="multipart/form-data" action="options.php">  
-      
+	 <div class="section panel">    
+      <form method="post" enctype="multipart/form-data">   
         <?php 
          /* settings_fields('cow4a_theme_options');  
           do_settings_sections($this->plugin_slug);*/
         ?>  
         <p class="submit">  
                 <input type="submit" class="button button-primary button-hero load-customize hide-if-no-customize" value="<?php _e('Create Page') ?>" />  
-        </p>      
-      
-      </form> 
-             
+        </p>       
+      </form>  
       <p>Created by <a href="http://www.web-4-all.cz">web-4-all.cz</a>.</p>
     </div>  
 </div>  
