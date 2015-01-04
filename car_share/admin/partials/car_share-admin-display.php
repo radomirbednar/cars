1b5a5bf50d11fd1e3105c3722bfa2b7c5271f9bb
ales <ales@ales-PC>
2015-01-04 18:18:14 +0100

    ~(~_~)~ some shortcode

--- car_share/admin/partials/car_share-admin-display.php
+++ car_share/admin/partials/car_share-admin-display.php
@@ -62,26 +62,25 @@ if (isset($_POST['createpage'])){
         $mi->attachIterator(new ArrayIterator($new_page_content));
         $mi->attachIterator(new ArrayIterator($new_page_template));
          
-        foreach( $mi as $value )
-        {     
-        list($new_page_title, $new_page_content, $new_page_template) = $value;                
-        //don't change the code bellow, unless you know what you're doing 
-        $page_check = get_page_by_title($new_page_title);
-        
-        $new_page = array(
-                'post_type' => 'page',
-                'post_title' => $new_page_title,
-                'post_content' => $new_page_content,
-                'post_status' => 'publish',
-                'post_author' => 1, 
-            ); 
-        
-        if(!isset($page_check->ID)){      
-                $new_page_id = wp_insert_post($new_page); 
-                if(!empty($new_page_template)){
-                        update_post_meta($new_page_id, '_wp_page_template', $new_page_template);
-                } 
-        }
+        foreach( $mi as $value ){     
+            list($new_page_title, $new_page_content, $new_page_template) = $value;                
+            //don't change the code bellow, unless you know what you're doing 
+            $page_check = get_page_by_title($new_page_title);
+
+            $new_page = array(
+                    'post_type' => 'page',
+                    'post_title' => $new_page_title,
+                    'post_content' => $new_page_content,
+                    'post_status' => 'publish',
+                    'post_author' => 1, 
+                ); 
+
+            if(!isset($page_check->ID)){      
+                    $new_page_id = wp_insert_post($new_page); 
+                    if(!empty($new_page_template)){
+                            update_post_meta($new_page_id, '_wp_page_template', $new_page_template);
+                    }
+            }
         }          
     } 
 ?> 
