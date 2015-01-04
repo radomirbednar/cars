915fc710e43787fcbb4e92b23536164862082e92
ales <ales@ales-PC>
2015-01-04 19:54:36 +0100

    ~(~_~)~ some shortcode

--- car_share/public/partials/shortcode/extras.php
+++ car_share/public/partials/shortcode/extras.php
@@ -4,7 +4,7 @@ $sc_options = get_option('sc-pages');
 $extras_car_url = isset($sc_options['checkout']) ? get_page_link($sc_options['checkout']) : '';
 
 ?>
-<form action="" method="post">
+<form action="<?php echo $extras_car_url ?>" method="post">
     
             <?php
             $args = array(
