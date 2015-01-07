2bdccacbdcf04008e954ad34a0365f33432e53f3
ales <ales@ales-PC>
2015-01-07 21:32:36 +0100

    stare nazvy inputu

--- car_share/admin/partials/car/single_car.php
+++ car_share/admin/partials/car/single_car.php
@@ -2,7 +2,7 @@
 <h2><?php _e('Pick-up location:', $this->car_share) ?></h2>
     <?php foreach ($locations as $location): ?>
         <label class="inline-label">
-            <input type="checkbox" name="_pickup_location[<?php echo $car_id ?>][]" value="<?php echo $location->ID ?>" <?php echo isset($pickup_location) && in_array($location->ID, $pickup_location) ? ' checked="checked" ' : '' ?>>
+            <input type="checkbox" name="car[<?php echo $car_id ?>][pickup_location][]" value="<?php echo $location->ID ?>" <?php echo isset($pickup_location) && in_array($location->ID, $pickup_location) ? ' checked="checked" ' : '' ?>>
             <?php _e($location->post_title) ?>
         </label>
     <?php endforeach; ?>
@@ -13,7 +13,7 @@
 <h2><?php _e('Drop-off Location:', $this->car_share) ?></h2>
     <?php foreach ($locations as $location): ?>
         <label class="inline-label">
-            <input type="checkbox" name="_dropoff_location[<?php echo $car_id ?>][]" value="<?php echo $location->ID ?>" <?php echo isset($dropoff_location) && in_array($location->ID, $dropoff_location) ? ' checked="checked" ' : '' ?>>
+            <input type="checkbox" name="car[<?php echo $car_id ?>][dropoff_location][]" value="<?php echo $location->ID ?>" <?php echo isset($dropoff_location) && in_array($location->ID, $dropoff_location) ? ' checked="checked" ' : '' ?>>
             <?php _e($location->post_title) ?>
         </label>
     <?php endforeach; ?>
