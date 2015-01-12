f5e961da02ef37ec17d94d233c6514d52ddc20ec
ales <ales@ales-PC>
2015-01-12 10:18:58 +0100

    some checkout fields...

--- car_share/admin/partials/car-category/day_prices.php
+++ car_share/admin/partials/car-category/day_prices.php
@@ -12,7 +12,7 @@ $days = get_days_of_week();
 
         <tr>
             <?php foreach ($days as $day_name => $label): ?>
-                <td><input type="text" name="_category_day_prices[<?php echo $day_name ?>]" class="day-price" value="<?php echo isset($category_day_prices[$day_name]) ? $category_day_prices[$day_name] : 0 ?>"></td>
+                <td><input type="number" step="0.01" name="_category_day_prices[<?php echo $day_name ?>]" class="day-price" value="<?php echo isset($category_day_prices[$day_name]) ? $category_day_prices[$day_name] : 0 ?>"></td>
             <?php endforeach; ?>
         </tr>
     </tbody>
