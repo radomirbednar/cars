41220815aa140f7d68c6f35ee26c8a1140c43f8e
brunofritsch <brunofritsch@10.0.0.13>
2015-01-14 13:21:32 +0100

    kdyz je vybrana pouze jedna agentura nezobrazuje se odkud a kam auto smeruje

--- car_share/admin/partials/car/single_car.php
+++ car_share/admin/partials/car/single_car.php
@@ -3,19 +3,32 @@
     <input type="text" name="car[<?php echo $car_id ?>][spz]" value="<?php echo isset($spz) ? esc_attr($spz) : '' ?>">
 </label>
 
+
+<?php
+ 
+    include('calendar_class.php'); 
+    $cal = new Calendar();
+    
+    $day_name_lengh=3;
+    
+    echo $cal->getMonthHTML(true, true, $day_name_lengh,false);
+
+?>
+
+
 <?php if (!empty($locations)): ?>
-<h2><?php _e('Pick-up location:', $this->car_share) ?></h2>
+    <h2><?php _e('Pick-up location:', $this->car_share) ?></h2>
     <?php foreach ($locations as $location): ?>
         <label class="inline-label">
             <input type="checkbox" name="car[<?php echo $car_id ?>][pickup_location][]" value="<?php echo $location->ID ?>" <?php echo isset($pickup_location) && in_array($location->ID, $pickup_location) ? ' checked="checked" ' : '' ?>>
             <?php _e($location->post_title) ?>
         </label>
     <?php endforeach; ?>
-    <div class="clear"></div> 
+    <div class="clear"></div>
 <?php endif; ?>
 
 <?php if (!empty($locations)): ?>
-<h2><?php _e('Drop-off Location:', $this->car_share) ?></h2>
+    <h2><?php _e('Drop-off Location:', $this->car_share) ?></h2>
     <?php foreach ($locations as $location): ?>
         <label class="inline-label">
             <input type="checkbox" name="car[<?php echo $car_id ?>][dropoff_location][]" value="<?php echo $location->ID ?>" <?php echo isset($dropoff_location) && in_array($location->ID, $dropoff_location) ? ' checked="checked" ' : '' ?>>
@@ -49,9 +62,9 @@
         <tr>
             <td colspan="4">
                 <button id="add-status-<?php echo $car_id ?>" data-car_id="<?php echo $car_id ?>" type="button" class="add-status alignleft button button-primary"><?php _e('Add status', $this->car_share) ?></button>
-            </td>    
-        </tr>    
-    </tfoot>    
+            </td>
+        </tr>
+    </tfoot>
 </table>
 
 <hr>
@@ -62,39 +75,38 @@
 
 <div class="clear"></div>
 <script>
-jQuery(document).ready(function ($) {
+    jQuery(document).ready(function($) {
 <?php
-
 if (!empty($statuses)):
     $status_key = 0;
     foreach ($statuses as $status):
-        
+
         $status = (object) $status;
-        
-        $date_from = isset($status->date_from) ? DateTime::createFromFormat('Y-m-d H:i:s', $status->date_from) : ''; 
-        $date_to = isset($status->date_to) ? DateTime::createFromFormat('Y-m-d H:i:s', $status->date_to) : '';        
-        ?>           
-            
-            var row = statusTableRow(
-                '<?php echo $car_id  ?>', 
-                <?php echo $status_key ?>, 
-                '<?php echo empty($date_from) ? '' : $date_from->format('d.m.Y') ?>', 
-                '<?php echo empty($date_from) ? '' : $date_from->format('H') ?>', 
-                '<?php echo empty($date_from) ? '' : $date_from->format('i') ?>', 
-                '<?php echo empty($date_to) ? '' : $date_to->format('d.m.Y') ?>', 
-                '<?php echo empty($date_to) ? '' : $date_to->format('H') ?>', 
-                '<?php echo empty($date_to) ? '' : $date_to->format('i') ?>');
-        
-            $('#post-body').find("#car-status-<?php echo $car_id ?> tbody").append(row);  
-            
-            var element = $("#car-status-<?php echo $car_id ?> tbody").find('.item:last');
-            apply_datepicker(element);            
-            
-            <?php
+
+        $date_from = isset($status->date_from) ? DateTime::createFromFormat('Y-m-d H:i:s', $status->date_from) : '';
+        $date_to = isset($status->date_to) ? DateTime::createFromFormat('Y-m-d H:i:s', $status->date_to) : '';
+        ?>
+
+                var row = statusTableRow(
+                        '<?php echo $car_id ?>',
+        <?php echo $status_key ?>,
+                        '<?php echo empty($date_from) ? '' : $date_from->format('d.m.Y') ?>',
+                        '<?php echo empty($date_from) ? '' : $date_from->format('H') ?>',
+                        '<?php echo empty($date_from) ? '' : $date_from->format('i') ?>',
+                        '<?php echo empty($date_to) ? '' : $date_to->format('d.m.Y') ?>',
+                        '<?php echo empty($date_to) ? '' : $date_to->format('H') ?>',
+                        '<?php echo empty($date_to) ? '' : $date_to->format('i') ?>');
+
+                $('#post-body').find("#car-status-<?php echo $car_id ?> tbody").append(row);
+
+                var element = $("#car-status-<?php echo $car_id ?> tbody").find('.item:last');
+                apply_datepicker(element);
+
+        <?php
         $status_key++;
-    endforeach; 
+    endforeach;
     ?>
-       status_key = <?php echo $status_key ?>;    
-    <?php endif; ?>
-});
+            status_key = <?php echo $status_key ?>;
+<?php endif; ?>
+    });
 </script>
